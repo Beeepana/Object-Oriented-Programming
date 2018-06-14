842b830ef6b089419e0e28b7d86fd954a24966fa
Beeepana <beeepanathakuri@gmail.com>
2018-06-14 06:40:50 +0545

    Input by user

---
+++ 2. Add two numbers..cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int m,n,sum=0;
+	cout<<"Enter two numbers to be added:";
+	cin>>m>>n;
+	sum=m+n;
+	cout<<"The sum of "<<m<<" and "<<n<<" is :"<<sum;
+}
